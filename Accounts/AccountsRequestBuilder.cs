7195057493877bdc910b8aa68276f480a9710866
Peter Soto <peter.soto@mccoys.com>
2025-06-27 12:30:11 -0500

    Added an accounts request builder

---
+++ Accounts/AccountsRequestBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itsm.Tdx.WebApi.Accounts;
+
+public class AccountsRequestBuilder : BaseRequestBuilder
+{
+    public AccountsRequestBuilder(TdxBaseClient client)
+    : base("accounts", client)
+    {
+
+    }
+    public async Task<List<Account>> GetAsync()
+    {
+        HttpRequestMessage request = new(HttpMethod.Get, Path);
+        request.Headers.Accept.Add(new("application/json"));
+        return await Client.SendRequestAsync<List<Account>>(request);
+    }
+    public AccountItemRequestBuilder this[string accountId]
+        => new("${Path}/{accountId}, Client");
+}
+
